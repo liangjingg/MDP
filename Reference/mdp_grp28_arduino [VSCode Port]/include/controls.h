2d503f05a3daff93626c520f4575edc23ef98c15
Serene Quek <48674425+serenequacked@users.noreply.github.com>
2021-04-16 16:34:31 +0800

    Delete Reference/mdp_grp28_arduino [VSCode Port] directory

--- Reference/mdp_grp28_arduino [VSCode Port]/include/controls.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <Arduino.h>
-#include <variables.h>
-#include "calibration.h"
-#include "variables.h"
-
-bool readCommand();
-void executeCommand();
-int convertCharToInt(char c);
